4b590e80164d8c2251c9b3540dba68dd6ca93ed6
zhang19960128 <zjh199601283492@gmail.com>
2023-02-06 15:09:38 -0500

    change

--- iterate.py
+++ iterate.py
@@ -13,9 +13,13 @@ class iteration:
         self.Echain.append(Efield); # E field has the unit Mv/cm
         self.Pchain.append(Position); # Position has the unit angstrom
         self.Polarization.append(Polar); # Polarization sequence. units C/m^2
-        self.axis = axis; # Axis for volume has the units Angstrom
+        self.axis = axis; # Axis for volume has the units Angstrom.
 
-    def nextsolution(self, dynmatrix):
+    def nextsolution(self, force, dynmatrix, atomcharge, edie, Polarnow):
+        self.Polarizaion.append(Polarnow);
+        DP = Polarnow - self.goal;
+        period = self.axis *( 10**(-10) )* 1.60217663 * ( 10**(-19) ) / ( 10**(-30) ) # Units C / m^2.
+        DP = DP - np.round(DP / period) * period;
         Amatrix = np.zeros((3 * self.natom + 3, 3 * self.natom + 3), dtype = float);
         x = np.zeros(3 * self.natom + 3);
         y = np.zeros(3 * self.natom + 3);
@@ -23,4 +27,16 @@ class iteration:
             for j in range(self.natom):
                 Amatrix[(3 * i) : (3 * i + 3), (3 * j) : (3 * j + 3)] = np.copy(dynmatrix[i][j]);
         for i in range(self.natom):
-            
+            Amatrix[(3 * i) : (3 * i + 3), (3 * self.natom) : (3 * self.natom + 3)] = np.copy( -1 * atomcharge[i] * 0.01); # Together makes the unit to eV / A when times the E field with Mv/cm.
+        for i in range(self.natom):
+            Amatrix[(3 * self.natom) : (3 *self.natom + 3), (3 * i) : (3 * i + 3)] = np.copy( -1 * atomcharge[i]);
+        Amatrix[(3 * self.natom) : (3 * self.natom + 3), (3 * self.natom) : (3 * self.natom + 3)] = np.copy(-1 * edie * 0.0000154961 * np.linalg.det(self.axis));
+        for i in range(self.natom):
+            for j in range(3):
+                y[i * 3 + j] = force[i][j];
+        y[(3 * self.natom) : (3 * self.natom + 3)] = np.linalg.det(self.axis) * DP;
+        x = np.matmul(np.linalg.inv(Amatrix), y);
+        dPosition = x[0 : (3 * self.natom)];
+        dEfield = x[(3 * self.natom) : (3 * self.natom + 3)];
+        self.Echain.append(self.Echain + dEfield);
+        self.Pchain.append(self.Pchain + dPosition);
