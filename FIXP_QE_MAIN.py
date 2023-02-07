a1655ab3ccb9d248e2827c955c960d560394a31d
zhang19960128 <zjh199601283492@gmail.com>
2023-02-06 22:33:15 -0500

    change

--- FIXP_QE_MAIN.py
+++ FIXP_QE_MAIN.py
@@ -5,16 +5,17 @@ import os
 if __name__=='__main__':
     natom = 5;
     workingfolder = './QECAL/';
-    pwcommand = "srun -n 48 {0:s}pw.x".format(workingfolder);
-    phcommand = "srun -n 48 {0:s}ph.x".format(workingfolder);
+    pwcommand = "/workspace/jiahaoz/PACKAGE_INSTALL/INTEL/mpi/2021.2.0/bin/mpirun -n 48 {0:s}pw.x".format(workingfolder);
+    phcommand = "/workspace/jiahaoz/PACKAGE_INSTALL/INTEL/mpi/2021.2.0/bin/mpirun -n 48 {0:s}ph.x".format(workingfolder);
+    dynmatfilename ="bto.dyn"
     startconfig = ABIIO(natom, workingfolder + 'btoE.in', workingfolder + 'bto.in');
     startconfig.obtaindipolescf(workingfolder + "btoE.out");
     startconfig.obtainforce(workingfolder + "btoE.out");
     startconfig.obtainefield(workingfolder + 'btoE.in');
     startPH = PHIO(natom);
     startPH.obtainph(workingfolder + 'ph.out');
-    startPH.obtaindyn(workingfolder + 'bto.dyn');
-    DeltaP = np.array([0.0, 0.0, 0.04]);
+    startPH.obtaindyn(workingfolder + dynmatfilename);
+    DeltaP = np.array([0.0, 0.0, 0.05]);
     Ptarget = DeltaP + startconfig.polarization;
     iter = iteration(natom, Ptarget, 0.00001, 0.00001);
     ITERMAXTIMES = 8;
@@ -23,9 +24,10 @@ if __name__=='__main__':
         iter.nextsolution(startconfig.force, startPH.dynmatrix, startPH.atomcharge, startPH.edie, startconfig.polarization);
         startconfig.writescfE(iter.Echain[-1], iter.Pchain[-1], workingfolder + "ITER{0:d}".format(i));
         startconfig.writenewscfnoe(iter.Pchain[-1], workingfolder + "ITERNOE{0:d}".format(i));
-        os.system("{0:s} < ITER{1:d} > {2:s}ITER.out{1:d}".format(pwcommand, i, workingfolder));
-        os.system("{0:s} < ITERNOE{1:d} > {2:s}ITERNOE.out{1:d}".format(pwcommand, i, workingfolder));
-        os.system("{0:s} < ph.in > {2:s}PHOUT{1:d}".format(phcommand, i, workingfolder));
+        os.system("{0:s} < {2:s}ITER{1:d} > {2:s}ITER.out{1:d}".format(pwcommand, i, workingfolder));
+        os.system("{0:s} < {2:s}ITERNOE{1:d} > {2:s}ITERNOE.out{1:d}".format(pwcommand, i, workingfolder));
+        os.system("{0:s} < {2:s}ph.in > {2:s}PHOUT{1:d}".format(phcommand, i, workingfolder));
+        os.system("cp {0:s}{2:s} DYNOUT{1:d}".format(workingfolder, i, dynmatfilename));
         nextconfig = ABIIO(natom, workingfolder + 'ITER{0:d}'.format(i), workingfolder + "ITERNOE{0:d}".format(i));
         nextconfig.obtaindipolescf(workingfolder + "ITER{0:d}.out".format(i));
         nextconfig.obtainforce(workingfolder + "ITER{0:d}.out".format(i));
